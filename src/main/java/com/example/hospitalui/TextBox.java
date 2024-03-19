fd3c521ac28c1d205694cf8c2b20c9d24ecd1aab
deep <deep.goyal@icloud.com>
2024-03-19 01:14:30 -0700

    eclipse init

--- src/main/java/com/example/hospitalui/TextBox.java
+++
@@ -1,5 +0,0 @@
-package com.example.hospitalui;
-
-public class TextBox {
-
-}
--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.hospitalui {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.hospitalui to javafx.fxml;
-    exports com.example.hospitalui;
-}
