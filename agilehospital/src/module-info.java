1a4da886e20ca39301cfb9eb96b08fcea053af44
Deep Goyal <deep.goyal@icloud.com>
2024-04-10 15:05:38 -0700

    module info and view connections

--- agilehospital/src/module-info.java
+++ agilehospital/src/module-info.java
@@ -1,6 +1,9 @@
 module agilehospital {
     exports application;
     exports application.serializedBackend;
+    exports application.FXMLHandlers to javafx.fxml;
+
+    opens application.FXMLHandlers to javafx.fxml;
 
     requires javafx.controls;
     requires javafx.fxml;
